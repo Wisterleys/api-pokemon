4c6b539696e8d150f9e9b906bd3bfb45d95c441a
Wisterleys <wister.sousa2@gmail.com>
2021-09-04 17:25:07 -0300

    Ajax configurado e já com URL

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-document.querySelector("search").addEventListener("blur",e=>{
+document.querySelector("#searchCLick").addEventListener("blur",e=>{
     const ajax = new XMLHttpRequest();
-    ajax.open("GET","",true)
+    ajax.open("GET","https://pokeapi.co/api/v2/pokemon/ditto",true)
+    ajax.send();
+
+    ajax.onload=e=>{
+        console.log(ajax.responseText)
+    }
 
 })
